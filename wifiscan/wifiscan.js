193fa9f23fcbe3b43712ce3583b8b816d1298189
arlemi <aturpaul@fr.ibm.com>
2016-05-05 21:21:19 +0200

    done

--- wifiscan/wifiscan.js
+++ wifiscan/wifiscan.js
@@ -10,13 +10,17 @@ module.exports = function(RED) {
 
 		node.on("input", function(msg) {
 			this.log('wifiscan - Input received');
+      		this.status({fill:'blue', shape:'ring', text:'requesting'});
 			wifiscanner.scan(function(err, data){
 			    if (err) {
-			        console.log("Error : " + err);
-			        return;
+		          msg.payload = 'Error: ' + err;
+		          node.send(msg);
+		          node.status({fill:'red', shape:'ring', text:'request error'});
+			    } else {
+		          msg.payload = data;
+		          node.send(msg);
+		          node.status({});
 			    }
-
-			    console.log(data);
 			});
 		});
 
